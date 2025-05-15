705e6284c91f349e5b8f5456edd4bf3ae277847b
quackandcheese <quackandcheeseart@gmail.com>
2025-05-15 07:19:03 -0500

    some changes

--- PogoMod/PogoPlugin.cs
+++ PogoMod/PogoPlugin.cs
@@ -11,7 +11,6 @@ using PogoMod.Modules;
 [module: UnverifiableCode]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 
-//rename this namespace
 namespace PogoMod
 {
     //[BepInDependency("com.rune580.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
@@ -19,14 +18,10 @@ namespace PogoMod
     [BepInPlugin(MODUID, MODNAME, MODVERSION)]
     public class PogoPlugin : BaseUnityPlugin
     {
-        // if you do not change this, you are giving permission to deprecate the mod-
-        //  please change the names to your own stuff, thanks
-        //   this shouldn't even have to be said
         public const string MODUID = "com.QuackAndCheese.PogoMod";
         public const string MODNAME = "PogoMod";
         public const string MODVERSION = "1.0.0";
 
-        // a prefix for name tokens to prevent conflicts- please capitalize all name tokens for convention
         public const string DEVELOPER_PREFIX = "QUACKANDCHEESE";
 
         public static PogoPlugin instance;
@@ -35,7 +30,6 @@ namespace PogoMod
         {
             instance = this;
 
-            //easy to use logger
             Log.Init(Logger);
 
             // used when you want to properly set up language folders
@@ -44,7 +38,6 @@ namespace PogoMod
             PogoAssets.Init(Asset.LoadAssetBundle("pogobundle"));
             StartCoroutine(PogoAssets.mainAssetBundle.UpgradeStubbedShadersAsync());
 
-            // character initialization
             new PogoSurvivor().Initialize();
 
             // make a content pack and add it. this has to be last
