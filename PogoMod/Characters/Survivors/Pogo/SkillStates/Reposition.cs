0d755aea343ce26e0b83a20bbe044c99981724b2
quackandcheese <quackandcheeseart@gmail.com>
2025-05-25 18:50:04 -0500

    change the math a lil on the reposition

--- PogoMod/Characters/Survivors/Pogo/SkillStates/Reposition.cs
+++ PogoMod/Characters/Survivors/Pogo/SkillStates/Reposition.cs
@@ -6,33 +6,36 @@ using RoR2;
 using UnityEngine;
 using System.Linq;
 using TMPro;
+using static Rewired.ComponentControls.Effects.RotateAroundAxis;
 
 namespace PogoMod.Characters.Survivors.Pogo.SkillStates
 {
 
     public class Reposition : BaseSkillState
     {
-        public static float duration = 1.0f;
+        public static float baseDuration = 2.0f;
+        public static float minVerticalVelocity = 6.0f;
+        public float timeToTarget = baseDuration;
 
         private CharacterMotor motor;
         private Vector3 startPosition;
         private HurtBox lockedTarget;
 
-        private Vector3 vxz;
+        private Vector3 initialVelocity;
 
-        private Vector3 CalculateProjectileVelocity(Vector3 start, Vector3 end, float timeToTarget)
+        private Vector3 CalculateProjectileVelocity(Vector3 startPos, Vector3 targetPos)
         {
-            Vector3 toTarget = end - start;
-            Vector3 toTargetXZ = new Vector3(toTarget.x, 0f, toTarget.z);
-
-            float yOffset = toTarget.y;
-            float xzDistance = toTargetXZ.magnitude;
-
-            vxz = toTargetXZ.normalized * (xzDistance / timeToTarget);
-            float vy = (yOffset + 0.5f * Mathf.Abs(Physics.gravity.y * motor.gravityScale) * timeToTarget * timeToTarget) / timeToTarget;
-
-            Vector3 v = new Vector3(vxz.x, vy, vxz.z);
-            return v;
+            Vector3 distanceVector = (targetPos - startPos);
+            Vector2 xzDistanceVec = new Vector2(distanceVector.x, distanceVector.z);
+            float distanceToTarget = xzDistanceVec.magnitude;
+            // timeToTarget is the amount of time the moving body will spend in the air, for fisherman it scales up to a maximum of 2 seconds based on distance. changing this will have a big effect on the feel of the arc
+            timeToTarget = Mathf.Min(distanceToTarget * 0.05f, baseDuration);
+            Vector2 xzDistanceVecNormalized = xzDistanceVec / distanceToTarget;
+            // Calculates the initial Y speed needed to reach the target Y position in the given time, resulting in a parabolic arc
+            float y = Trajectory.CalculateInitialYSpeed(timeToTarget, distanceVector.y);
+            float travelRate = distanceToTarget / timeToTarget;
+            initialVelocity = new Vector3(xzDistanceVecNormalized.x * travelRate, y, xzDistanceVecNormalized.y * travelRate);
+            return initialVelocity;
         }
 
         public override void OnEnter()
@@ -48,16 +51,16 @@ namespace PogoMod.Characters.Survivors.Pogo.SkillStates
 
             if (lockedTarget)
             {
-                base.characterMotor.Motor.ForceUnground();
-
                 Vector3 targetPosition = lockedTarget.transform.position;
-                Vector3 velocity = CalculateProjectileVelocity(startPosition, targetPosition, duration);
+                Vector3 velocity = CalculateProjectileVelocity(startPosition, targetPosition);
+
+                motor.Motor.ForceUnground();
+                motor.disableAirControlUntilCollision = true;
+                motor.velocity = Vector3.zero;
                 motor.velocity = velocity;
             }
-
             else
             {
-                // No target found: exit early
                 outer.SetNextStateToMain();
             }
         }
@@ -65,13 +68,13 @@ namespace PogoMod.Characters.Survivors.Pogo.SkillStates
         public override void FixedUpdate()
         {
             base.FixedUpdate();
-            if (fixedAge >= duration || !lockedTarget)
+            if (fixedAge >= timeToTarget || !lockedTarget || Vector3.Distance(lockedTarget.transform.position, transform.position) <= 3.0)
             {
                 outer.SetNextStateToMain();
             }
 
             // Keep XZ velocity constant and add homing.
-            Vector3 toTargetXZ = lockedTarget.transform.position - base.characterBody.corePosition;
+            Vector3 toTargetXZ = lockedTarget.transform.position - transform.position;
             toTargetXZ.y = 0f;
 
             if (toTargetXZ != Vector3.zero)
@@ -82,7 +85,7 @@ namespace PogoMod.Characters.Survivors.Pogo.SkillStates
                 float homingStrength = 5f; // tweak this
                 Vector3 newDir = Vector3.RotateTowards(currentDir, desiredDir, homingStrength * Time.fixedDeltaTime, 0f);
 
-                float timeLeft = Mathf.Max(duration - fixedAge, Time.fixedDeltaTime);
+                float timeLeft = Mathf.Max(timeToTarget - fixedAge, Time.fixedDeltaTime);
                 float distanceXZ = toTargetXZ.magnitude;
                 float speed = distanceXZ / timeLeft;
 
@@ -92,8 +95,10 @@ namespace PogoMod.Characters.Survivors.Pogo.SkillStates
 
         public override void OnExit()
         {
+            motor.disableAirControlUntilCollision = false;
+            motor.velocity = Vector3.zero;
+
             base.OnExit();
-            if (motor) motor.velocity = Vector3.zero;
         }
 
         private HurtBox FindTarget()
